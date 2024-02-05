273ded9b83f2b6f78a537db7eb46f3ed62e0eafb
ASE <wey3797@naver.com>
2024-02-05 22:57:24 +0900

    CommCdVO 수정

--- src/main/java/com/changsoo/copypastestudy/commCd/vo/CommCdVO.java
+++ src/main/java/com/changsoo/copypastestudy/commCd/vo/CommCdVO.java
@@ -6,4 +6,16 @@ import org.apache.ibatis.type.Alias;
 @Data
 @Alias("commCdVO")
 public class CommCdVO {
+    private String commCd;
+    private String commNm;
+    private String commDesc;
+    private String createUserId;
+    private String createDt;
+
+    private String commDtlCd;
+    private String commDtlNm;
+    private String commDtlDesc;
+    private String useYn;
+    private String updateUserId;
+    private String updateDt;
 }
