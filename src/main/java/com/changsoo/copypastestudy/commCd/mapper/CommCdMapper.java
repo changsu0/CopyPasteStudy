618638883bf1b69c6a6e02abda5fef80b0335f64
YooJisu <jisu6357@naver.com>
2024-02-05 23:54:05 +0900

    commCd

--- src/main/java/com/changsoo/copypastestudy/commCd/mapper/CommCdMapper.java
+++ src/main/java/com/changsoo/copypastestudy/commCd/mapper/CommCdMapper.java
@@ -3,10 +3,22 @@ package com.changsoo.copypastestudy.commCd.mapper;
 import com.changsoo.copypastestudy.ase.vo.AseJqDataVO;
 import com.changsoo.copypastestudy.ase.vo.AseMemberVO;
 import com.changsoo.copypastestudy.commCd.vo.CommCdVO;
+import com.changsoo.copypastestudy.commCd.vo.CommDtlCdVO;
+import com.changsoo.copypastestudy.yjs.vo.YjsCommDtVO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface CommCdMapper {
+    public List<CommCdVO> selectCommCdListYjs(CommCdVO commCdVO);
+    public CommCdVO selectCommCdOneYjs(CommCdVO commCdVO);
+    public int insertCommCdYjs(CommCdVO commCdVO);
+    public int updateCommCdYjs(CommCdVO commCdVO);
+    public int deleteCommCdYjs(CommCdVO commCdVO);
+    public List<CommDtlCdVO> selectCommDtListYjs(CommDtlCdVO commDtlCdVO);
+    public CommDtlCdVO selectCommDtOneYjs(CommDtlCdVO commDtlCdVO);
+    public int insertCommDtYjs(CommDtlCdVO commDtlCdVO);
+    public int updateCommDtYjs(CommDtlCdVO commDtlCdVO);
+    public int deleteCommDtYjs(CommDtlCdVO commDtlCdVO);
 }
--- src/main/java/com/changsoo/copypastestudy/commCd/service/CommCdService.java
+++ src/main/java/com/changsoo/copypastestudy/commCd/service/CommCdService.java
@@ -4,6 +4,11 @@ import com.changsoo.copypastestudy.ase.mapper.AseCatgMapper;
 import com.changsoo.copypastestudy.ase.service.AseCatgService;
 import com.changsoo.copypastestudy.ase.vo.AseCatgVO;
 import com.changsoo.copypastestudy.commCd.mapper.CommCdMapper;
+import com.changsoo.copypastestudy.commCd.vo.CommCdVO;
+import com.changsoo.copypastestudy.commCd.vo.CommDtlCdVO;
+import com.changsoo.copypastestudy.yjs.mapper.YjsDataTablesMapper;
+import com.changsoo.copypastestudy.yjs.vo.YjsDataTablesVO;
+import com.changsoo.copypastestudy.yjs.vo.YjsMemberVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +20,46 @@ public class CommCdService {
 
     public CommCdService(CommCdMapper commCdMapper) {
         this.commCdMapper = commCdMapper;
+    }
+
+    @Override
+    public List<CommCdVO> selectCommCdListYjs(CommCdVO commCdVO){
+        return commCdMapper.selectCommCdListYjs(commCdVO);
+    }
+
+    @Override
+    public CommCdVO selectCommCdOneYjs(CommCdVO commCdVO){
+        return commCdMapper.selectCommCdOneYjs(commCdVO);
+    }
+
+    @Override
+    public int insertCommCdYjs(CommCdVO commCdVO) {
+        return commCdMapper.insertCommCdYjs(commCdVO);
+    }@Override
+    public int updateCommCdYjs(CommCdVO commCdVO) {
+        return commCdMapper.updateCommCdYjs(commCdVO);
+    }@Override
+    public int deleteCommCdYjs(CommCdVO commCdVO) {
+        return commCdMapper.deleteCommCdYjs(commCdVO);
+    }
+    @Override
+    public List<CommDtlCdVO> selectCommDtListYjs(CommDtlCdVO commDtlCdVO){
+        return commCdMapper.selectCommDtListYjs(commDtlCdVO);
+    }
+
+    @Override
+    public CommDtlCdVO selectCommDtOneYjs(CommDtlCdVO commDtlCdVO){
+        return commCdMapper.selectCommDtOneYjs(commDtlCdVO);
+    }
+
+    @Override
+    public int insertCommDtYjs(CommDtlCdVO commDtlCdVO) {
+        return commCdMapper.insertCommDtYjs(commDtlCdVO);
+    }@Override
+    public int updateCommDtYjs(CommDtlCdVO commDtlCdVO) {
+        return commCdMapper.updateCommDtYjs(commDtlCdVO);
+    }@Override
+    public int deleteCommDtYjs(CommDtlCdVO commDtlCdVO) {
+        return commCdMapper.deleteCommDtYjs(commDtlCdVO);
     }
 }
