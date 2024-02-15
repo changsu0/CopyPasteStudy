0c5ea8d99e3afa4218420ac03fac9ed3a8e28db5
ASE <wey3797@naver.com>
2024-02-15 22:23:17 +0900

    공통코드 ASE 컨트롤러 수정

--- src/main/java/com/changsoo/copypastestudy/commCd/controller/CommCdController.java
+++ src/main/java/com/changsoo/copypastestudy/commCd/controller/CommCdController.java
@@ -1,65 +1,105 @@
- package com.changsoo.copypastestudy.commCd.controller;
+package com.changsoo.copypastestudy.commCd.controller;
 
- import com.changsoo.copypastestudy.commCd.service.CommCdService;
- import com.changsoo.copypastestudy.commCd.vo.CommCdVO;
- import com.google.gson.Gson;
- import org.springframework.stereotype.Controller;
- import org.springframework.ui.Model;
- import org.springframework.web.bind.annotation.GetMapping;
- import org.springframework.web.bind.annotation.ModelAttribute;
- import org.springframework.web.bind.annotation.ResponseBody;
+import com.changsoo.copypastestudy.commCd.service.CommCdService;
+import com.changsoo.copypastestudy.commCd.vo.CommCdVO;
+import com.changsoo.copypastestudy.commCd.vo.CommDtlCdVO;
+import com.google.gson.Gson;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
- import java.util.HashMap;
- import java.util.List;
+import java.util.HashMap;
+import java.util.List;
 
- @Controller
- public class CommCdController {
-     final CommCdService commCdService;
+@Controller
+public class CommCdController {
+    final CommCdService commCdService;
 
-     public CommCdController(CommCdService commCdService) {
-         this.commCdService = commCdService;
-     }
+    public CommCdController(CommCdService commCdService) {
+        this.commCdService = commCdService;
+    }
 
-     @GetMapping("/commCdListYjs")
-     public String commCdListYjs(Model model){
+    @GetMapping("/commCdListYjs")
+    public String commCdListYjs(Model model){
 
-         return "yjs/tables/yjsDataTables02";
-     }
+        return "yjs/tables/yjsDataTables02";
+    }
 
-     @GetMapping("/selectCommCdListYjs")
-     @ResponseBody
-     public String selectCommCdListYjs(@ModelAttribute CommCdVO commCdVO){
-         String rstJson = null;
-         List<CommCdVO> selectCommCdListYjs = commCdService.selectCommCdListYjs(commCdVO);
+    @GetMapping("/selectCommCdListYjs")
+    @ResponseBody
+    public String selectCommCdListYjs(@ModelAttribute CommCdVO commCdVO){
+        String rstJson = null;
+        List<CommCdVO> selectCommCdListYjs = commCdService.selectCommCdListYjs(commCdVO);
 
-         HashMap map = new HashMap();
-         map.put("data", selectCommCdListYjs);
+        HashMap map = new HashMap();
+        map.put("data", selectCommCdListYjs);
 
-         Gson gson = new Gson();
-         rstJson = gson.toJson(map);
+        Gson gson = new Gson();
+        rstJson = gson.toJson(map);
 
-         return rstJson;
-     }
+        return rstJson;
+    }
 
-     @GetMapping("/commCdListAse")
-     public String commCdListAse(Model model){
+    @GetMapping("/commCdListAse")
+    public String commCdListAse(Model model){
 
-         return "ase/dataTable/aseDataTable01";
-     }
+        return "ase/dataTable/aseDataTable01";
+    }
 
-     @GetMapping("/selectCommCdListAse")
-     @ResponseBody
-     public String selectCommCdListAse(@ModelAttribute CommCdVO commCdVO){
+    @GetMapping("/selectCommCdListAse")
+    @ResponseBody
+    public String selectCommCdListAse(@ModelAttribute CommCdVO commCdVO){
+        String rstJson = null;
+        List<CommCdVO> selectCommCdListAse = commCdService.selectCommCdListAse(commCdVO);
 
-         String rstJson = null;
-         List<CommCdVO> selectCommCdListAse = commCdService.selectCommCdListAse(commCdVO);
+        HashMap map = new HashMap();
+        map.put("data", selectCommCdListAse);
 
-         HashMap map = new HashMap();
-         map.put("data", selectCommCdListAse);
+        Gson gson = new Gson();
+        rstJson = gson.toJson( map );
 
-         Gson gson = new Gson();
-         rstJson = gson.toJson( map );
+        return rstJson;
+    }
 
-         return rstJson;
-     }
- }
+    @GetMapping("/selectCommDtlCdListAse")
+    @ResponseBody
+    public String selectCommDtlCdListAse(@ModelAttribute CommDtlCdVO commDtlCdVO){
+        String rstJson = null;
+        List<CommDtlCdVO> selectCommDtlCdListAse = commCdService.selectCommDtlCdListAse(commDtlCdVO);
+
+        HashMap map = new HashMap();
+        map.put("data", selectCommDtlCdListAse);
+
+        Gson gson = new Gson();
+        rstJson = gson.toJson( map );
+
+        return rstJson;
+    }
+
+    @PostMapping("/selectCommCdOneAse")
+    @ResponseBody
+    public String selectCommCdOneAse(@RequestBody CommCdVO commCdVO){
+        String rstJson = null;
+
+        Gson gson = new Gson();
+        rstJson = gson.toJson(commCdService.selectCommCdOneAse(commCdVO) );
+
+        return rstJson;
+    }
+
+    @PostMapping("/selectCommDtlCdOneAse")
+    @ResponseBody
+    public String selectCommDtlCdOneAse(@RequestBody CommDtlCdVO commDtlCdVO){
+        String rstJson = null;
+        CommCdVO selectCommDtlCdOneAse = commCdService.selectCommDtlCdOneAse(commDtlCdVO);
+
+        HashMap map = new HashMap();
+        map.put("data", selectCommDtlCdOneAse);
+
+        Gson gson = new Gson();
+        rstJson = gson.toJson( map );
+
+        return rstJson;
+    }
+
+}
